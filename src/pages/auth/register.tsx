885782235e4e54608bf511959f39af7497deba6e
Juan Pablo Rodriguez <96890522+krosul@users.noreply.github.com>
2023-02-22 22:42:24 -0500

    validations in page register

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,69 +1,126 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NextLink from 'next/link';
-import { AuthLayout } from '@/components/Layouts';
-import {
-  Box,
-  Grid,
-  Typography,
-  TextField,
-  Button,
-  Link,
-  Divider,
-} from '@mui/material';
+import {AuthLayout} from '@/components/Layouts';
+import {Box, Grid, Typography, TextField, Button, Link, Divider, Chip} from '@mui/material';
+import {useForm} from 'react-hook-form';
+import {tesloApi} from 'api';
+import {validations} from 'utils';
+import {ErrorOutline} from '@mui/icons-material';
+
+type FormData = {
+  name: string;
+  email: string;
+  password: string;
+};
 
 const RegisterPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<FormData>();
+
+  const [showMessageError, setShowMessageError] = useState(false);
+
+  const onRegisterUser = async ({email, password, name}: FormData) => {
+    setShowMessageError(false);
+    console.log(email, name, password);
+
+    try {
+      const {data} = await tesloApi.post('/user/register', {email, password, name});
+      console.log({data});
+    } catch (err) {
+      setShowMessageError(true);
+
+      setTimeout(() => setShowMessageError(false), 3000);
+      console.log(err);
+    }
+  };
+
   return (
-    <AuthLayout title="Inicia sesion">
-      <Box sx={{ width: 350, padding: '10px 20px' }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="h1" component="h1">
-              Registrate
-            </Typography>
+    <AuthLayout title="Registrate">
+      <form onSubmit={handleSubmit(onRegisterUser)}>
+        <Box sx={{width: 350, padding: '10px 20px'}}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h1" component="h1">
+                Registrate
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Nombre"
+                variant="filled"
+                fullWidth
+                {...register('name', {
+                  required: 'Este campo es requerido',
+                  minLength: {value: 2, message: 'Minimo 2 caracteres'},
+                })}
+                error={!!errors.name}
+                helperText={errors.name?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Correo"
+                variant="filled"
+                fullWidth
+                {...register('email', {
+                  required: 'Este campo es requerido',
+                  validate: validations.isEmail,
+                })}
+                error={!!errors.email}
+                helperText={errors.email?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Contraseña"
+                type="password"
+                variant="filled"
+                fullWidth
+                {...register('password', {
+                  required: 'Este campo es requerido',
+                  minLength: {value: 6, message: 'Minimo 6 caracteres'},
+                })}
+                error={!!errors.password}
+                helperText={errors.password?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              {showMessageError && (
+                <Chip
+                  label="Lo sentimos algo ha salido mal"
+                  color="error"
+                  icon={<ErrorOutline />}
+                  className="fadeIn"
+                  sx={{display: 'flex', padding: 1, marginY: 0.5}}
+                />
+              )}
+              <Button
+                color="secondary"
+                className="circular-btn"
+                size="large"
+                fullWidth
+                type="submit"
+                disabled={showMessageError}
+              >
+                Registrarse
+              </Button>
+            </Grid>
+            <Grid item xs={12} display="flex" justifyContent="center" alignItems="center" gap={2}>
+              <Divider sx={{width: '45%', fontSize: '15px'}} />
+              <Typography variant="h6">O</Typography>
+              <Divider sx={{width: '45%', fontSize: '15px'}} />
+            </Grid>
+            <Grid item xs={12} display="grid" justifyItems="center">
+              <NextLink href="/auth/login" passHref legacyBehavior>
+                <Link underline="always">Inicia sesion</Link>
+              </NextLink>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <TextField label="Nombre" variant="filled" fullWidth />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField label="Correo" variant="filled" fullWidth />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              label="Contraseña"
-              type="password"
-              variant="filled"
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              color="secondary"
-              className="circular-btn"
-              size="large"
-              fullWidth
-            >
-              Registrarse
-            </Button>
-          </Grid>
-          <Grid
-            item
-            xs={12}
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            gap={2}
-          >
-            <Divider sx={{ width: '45%', fontSize: '15px' }} />
-            <Typography variant="h6">O</Typography>
-            <Divider sx={{ width: '45%', fontSize: '15px' }} />
-          </Grid>
-          <Grid item xs={12} display="grid" justifyItems="center">
-            <NextLink href="/auth/login" passHref legacyBehavior>
-              <Link underline="always">Inicia sesion</Link>
-            </NextLink>
-          </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </form>
     </AuthLayout>
   );
 };
